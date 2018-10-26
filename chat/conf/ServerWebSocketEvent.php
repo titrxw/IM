5cbfb2df65138d1124d3e5fc11202660326cf23c
rxw <1120309488@qq.com>
2018-10-26 10:09:54 +0800

    Update ServerWebSocketEvent.php

--- chat/conf/ServerWebSocketEvent.php
+++ chat/conf/ServerWebSocketEvent.php
@@ -51,7 +51,7 @@ class ServerWebSocketEvent implements SwooleEvent
                     Container::getInstance()->getComponent($key, 'meedo')->pdo->getAttribute(\PDO::ATTR_SERVER_INFO);
                 }
             } catch (\Throwable $e) {
-                $this->handleThrowable($e);
+                Container::getInstance()->getComponent(SYSTEM_APP_NAME, 'exception')->handleException($e);
             }
         });
     }
