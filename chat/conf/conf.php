71968ef60c3d60032d4889588f436e8c616107d6
titrxw <1120309488@qq.com>
2018-11-29 23:07:21 +0800

    change

--- chat/conf/conf.php
+++ chat/conf/conf.php
@@ -14,8 +14,6 @@ return array(
     'addComponentsMap' => array(
         'validate' => 'framework\\components\\validate\\Validate',
         'password' => 'framework\\components\\security\\Password',
-        'tokenBucket' => 'framework\\tokenbucket\\Bucket',
-        'apireset' => 'blog\\lib\\ApiReset',
         'redis' => 'framework\\components\\cache\\Redis',
         'uniqueid' => 'framework\\components\\uniqueid\\UniqueId',
         // 'wsevent' => 'chat\\conf\\WsEvent'
@@ -49,49 +47,6 @@ return array(
         
             // [optional] Enable logging (Logging is disabled by default for better performance)
             'logging' => true,
-        ),
-        'dispatcher' => array(
-            'action' => array(
-                'prefix' => '',
-                'suffix' => 'Api'
-            )
-        ),
-        'tokenBucket' => [
-            'buckets' => [
-                'request' => [
-                    'class' => 'framework\\tokenbucket\\Request',
-                    'auto' => true,
-                    'conf' => [
-                        'max' => 20,
-                        'key' => 'bucket_b_request',
-                        'range' => 60, //单位s
-                        'addStep' => 0,
-                        'timeStep' => 3//单位s
-                    ]
-                ],
-                'mobile' => [
-                    'class' => 'framework\\tokenbucket\\Mobile',
-                    'auto' => false,
-                    'conf' => [
-                        'max' => 5,
-                        'key' => 'mobile',
-                        'range' => 60, //单位s
-                        'addStep' => 0,
-                        'timeStep' => 3//单位s
-                    ]
-                ],
-                'ip' => [
-                    'class' => 'framework\\tokenbucket\\Ip',
-                    'auto' => false,
-                    'conf' => [
-                        'max' => 3,
-                        'key' => 'ip',
-                        'range' => 5, //单位s
-                        'addStep' => 0,
-                        'timeStep' => 3//单位s
-                    ]
-                ]
-            ],
-        ]
+        )
     )
 );
